f346c0771a59ad48b3889b43b6458ad7c426ee2c
animeshux <animeshux@gmail.com>
2018-04-17 23:21:37 -0700

    updated constants

--- touchpoint_experiment/constants.js
+++ touchpoint_experiment/constants.js
@@ -1,6 +1,6 @@
 var stageWidth = 800;
 var stageHeight = 800;
-var center = [6.89874, 184.457, -76.7029];
+var center = [14.729, 171.341, -77.4039];
 var widthScaler = 1.4;
 var heightScaler = 1.4;
 var tipPosition;
